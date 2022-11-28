0f8679a38b9428ad96dde4705250fd3b3398fe62
opilane <opilane@HP-CZC6297BB7>
2022-11-28 17:12:09 +0200

    Add project files.

---
+++ Tsüklid.py
@@ -0,0 +1,28 @@
+﻿j=0
+i=0
+while i<15:
+    i+=1
+    a=float(input(f"{i} Sisesta A: "))
+    if int(a)==a: j+=1 
+print(j)
+
+j=0
+i=0
+while True:
+    i+=1
+    a=float(input(f"{i} Sisesta A: "))
+    if int(a)==a: j+1
+    if i==15: break
+print(j)
+
+from math import *
+j=0
+for i in range(0,15,1): #for i in range(15):
+    a=float(input(f"{i+1} Sisesta A: "))
+    if int(a)==a:
+        j+=1
+print(j)
+
+
+
+
